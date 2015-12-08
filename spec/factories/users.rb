7030bb8259498e3d83025c1433b3f8114e8fb049
Erin Isbell <erinmarie.isbell@gmail.com>
2015-12-08 10:29:58 -0600

    Rolledback

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
   factory :user do
     name "MyString"
-email "MyString"
-password_digest "MyString"
-location "MyString"
-fan false
-band false
+    email "MyString"
+    password_digest "MyString"
+    location "MyString"
+    fan false
+    band false
   end
 
 end
