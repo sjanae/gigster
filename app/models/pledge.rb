e2fe0f4f66bd6414b48989f6682b4f7bfab42226
Shatavia <shataviajv@live.com>
2015-12-11 17:24:38 -0600

    updated controller and models

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,4 +1,11 @@
 class Pledge < ActiveRecord::Base
-	belongs_to :concert
-	belongs_to :user, type: :fan
-end
+  belongs_to :concert
+  belongs_to :user, type: :fan
+  validates :price, presence: true
+
+  end
+
+  
+
+
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
-  resources :pledges, except: [:new, :edit]
-  resources :concerts, except: [:new, :edit]
-  resources :users, except: [:new, :edit]
-  resources :fans
-  resources :bands
-  use_doorkeeper
+   use_doorkeeper
+  
+
+    resources :users, except: [:new, :edit]
+    resources :fans
+    resources :bands
+ 
+
+  resources :concerts do
+    resources :pledges, only: [:new, :create, :update, :destroy]
+  end
+
 end
+
+
+# nested routes = http..something.com/concerts/concert_id(number)/pledges
+
+
+# something.com/fans/1/pledges
+# something.com/bands/1/concerts
