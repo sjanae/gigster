b7a378d0994a192e7057684bd44995aab75d9ca9
Shatavia <shataviajv@live.com>
2015-12-14 13:45:10 -0600

    update relationships for all models

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,5 +1,5 @@
 class Concert < ActiveRecord::Base
-  has_and_belongs_to_many :users, type: :band
+  has_and_belongs_to_many :bands
   has_many :pledges
   validates :location, :venue, presence: true
 end
--- app/models/fan.rb
+++ app/models/fan.rb
@@ -1,5 +1,6 @@
 class Fan < ActiveRecord::Base
   has_many :pledges
+  has_many :concerts => :pledges
   validates :location, presence: true
 
   end
