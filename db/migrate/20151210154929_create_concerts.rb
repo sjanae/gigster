f52cd0c2e8362879c9155a8dda50d2974c083e1d
Shatavia <shataviajv@live.com>
2015-12-14 14:04:18 -0600

    Remove the final single table inheritance relic. BOOOOO single table inheritance BOOOO

--- db/migrate/20151210154929_create_concerts.rb
+++ db/migrate/20151210154929_create_concerts.rb
@@ -2,7 +2,7 @@ class CreateConcerts < ActiveRecord::Migration
   def change
     create_table :concerts do |t|
 
-      t.references :user, type: :band, index: true, foreign_key: true # attaches band to concert
+      t.references :user, index: true, foreign_key: true # attaches band to concert
       t.string :location
       t.string :venue
       t.string :success, default: "pending"
