608612c221703c672c969abeb24dd4eb3a202008
Erin Isbell <erinmarie.isbell@gmail.com>
2015-12-17 14:55:33 -0600

    Showing Concerts on a certain band

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -10,8 +10,10 @@ class Concert < ActiveRecord::Base
   def status
     if votes_for.size < 1
       return "We need more pledges!"
+      UserMailer.send_unsuccess(@pledges.id).delivernow
     else
       return "This concert is DEFINITELY a thing!"
+      UserMailer.send_success(@pledges.id).delivernow
     end
   end
 
--- app/serializers/concert_serializer.rb
+++ app/serializers/concert_serializer.rb
@@ -1,4 +1,4 @@
 class ConcertSerializer < ActiveModel::Serializer
   attributes :id, :location, :venue, :pledge, :status, :performance_date, :funding_goal
-  
+
 end
