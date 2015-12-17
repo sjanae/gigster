f4b0c3f923e0db4bd2d2c0c2b3c9578b64e41e83
Shatavia <shataviajv@live.com>
2015-12-17 13:33:11 -0600

    removed voter_id

--- app/models/band.rb
+++ app/models/band.rb
@@ -2,7 +2,7 @@ class Band < ActiveRecord::Base
   acts_as_votable
   has_many :concerts
   has_one :user
-  validates_uniqueness_of :voter_id
+  # validates_uniqueness_of :voter_id
   # validates :name, :location, :video_url, :audio_url, presence: true
 
   def fanship
