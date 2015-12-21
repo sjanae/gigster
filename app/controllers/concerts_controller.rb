dfbb4eb22c11a54ce8a8c4136b0a32d731e21224
Erin Isbell <erinmarie.isbell@gmail.com>
2015-12-21 10:46:25 -0600

    Concert

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -40,7 +40,7 @@ class ConcertsController < ApplicationController
   end
 
   def destroy
-    @concert = current_user.band.concert
+    @concert = current_user.band
     if @concert.destroy
     else
       render json: @concert.errors, status: :unauthorized
@@ -57,7 +57,7 @@ class ConcertsController < ApplicationController
   end
 
 
-  
+
   private
 
   def set_concert
